7dbebd276e912aa20f2d54fae7d73f28908133e2
Daria Ishchenko <54391498+Nekttuman@users.noreply.github.com>
2020-01-30 19:01:58 +0900

    Scenes prototype add
    created Main, Tracking and Themes Menu scenes
    also:
    -switch platform (Android)
    -created scenes switch script

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadScene : MonoBehaviour
+{
+    public Button CurrButton;
+    public string sceneName;
+
+    void Start()
+    {
+        Button btn = CurrButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        Application.LoadLevel(sceneName);
+    }
+}
