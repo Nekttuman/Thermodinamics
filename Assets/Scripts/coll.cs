35ff95964140db975349cc5e5d704eaf2f526ee5
Daria Ishchenko <54391498+Nekttuman@users.noreply.github.com>
2020-02-10 01:00:16 +0900

    -
    -

---
+++ Assets/Scripts/coll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coll : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter()
+    {
+        Debug.Log("Hit Something");  // Передаем сообщение в консоль Unity  
+    }
+}
