0a8479095bd90241375112b0b3c80e516fcd7310
Daria Ishchenko <54391498+Nekttuman@users.noreply.github.com>
2020-02-13 01:07:35 +0900

    -
    -

---
+++ Assets/Opener_text.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opener_text : MonoBehaviour
+{
+    public GameObject panel;
+    public void openConsp()
+    {
+        if (panel != null)
+        {
+            Animator animator = panel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("open");
+                animator.SetBool("open", !isOpen);
+            }
+        }
+    }
+}
