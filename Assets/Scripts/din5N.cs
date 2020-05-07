76f895f67ff90b3be16e44bd541a3341f973a847
Daria Ishchenko <54391498+Nekttuman@users.noreply.github.com>
2020-05-07 22:31:20 +0900

    dinamometer 5N added

--- Assets/Scripts/din5N.cs
+++ Assets/Scripts/din5N.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dianmometer_position : MonoBehaviour
+public class din5N : MonoBehaviour
 {
     GameObject pointer;
-    const float nullPosition = 4.83f;
-    const float onePosition = -7.97f;
+    const float nullPosition = 4.79f;
+    const float onePosition = -7.92f;
 
     const float minPosition = 5.43f;
     const float maxPosition = -8.22f;
@@ -15,10 +14,10 @@ public class dianmometer_position : MonoBehaviour
     public float setPosition = 0f;
 
     float error = 0;
-    
+
     void Start()
     {
-        pointer = GameObject.Find("pointer");
+        pointer = GameObject.Find("pointer1");
         pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, nullPosition, pointer.transform.localPosition.z);
         error = UnityEngine.Random.Range(0, 0.01f);
         Debug.Log(error);
@@ -27,22 +26,22 @@ public class dianmometer_position : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-// Debug.Log(pointer.transform.localPosition.y);
+        //Debug.Log(pointer.transform.localPosition.y);
 
         if (setPosition < 0)
         {
             pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, minPosition, pointer.transform.localPosition.z);
             Debug.Log("Out of range < 0");
         }
-        else if (setPosition > 1)
+        else if (setPosition > 5)
         {
             pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, maxPosition, pointer.transform.localPosition.z);
             Debug.Log("Out of range > 1");
         }
         else
         {
-            
-            pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, nullPosition - 1.28f * 10 * (setPosition + error), pointer.transform.localPosition.z);
+
+            pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, nullPosition - 1.28f * 10 * (setPosition/5f + error), pointer.transform.localPosition.z);
         }
 
 
--- Assets/Scripts/dinamometerPosition5N.cs
+++ Assets/Scripts/dinamometerPosition5N.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dianmometer_position : MonoBehaviour
+public class dinanmometerPosition5N : MonoBehaviour
 {
     GameObject pointer;
     const float nullPosition = 4.83f;
@@ -15,21 +15,21 @@ public class dianmometer_position : MonoBehaviour
     public float setPosition = 0f;
 
     float error = 0;
-    
+
     void Start()
     {
         pointer = GameObject.Find("pointer");
         pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, nullPosition, pointer.transform.localPosition.z);
-        error = UnityEngine.Random.Range(0, 0.01f);
+        error = UnityEngine.Random.Range(0, 0.02f);
         Debug.Log(error);
     }
 
     // Update is called once per frame
     void Update()
     {
-// Debug.Log(pointer.transform.localPosition.y);
+         Debug.Log(pointer.transform.localPosition.y);
 
-        if (setPosition < 0)
+        /*if (setPosition < 0)
         {
             pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, minPosition, pointer.transform.localPosition.z);
             Debug.Log("Out of range < 0");
@@ -41,9 +41,9 @@ public class dianmometer_position : MonoBehaviour
         }
         else
         {
-            
+
             pointer.transform.localPosition = new Vector3(pointer.transform.localPosition.x, nullPosition - 1.28f * 10 * (setPosition + error), pointer.transform.localPosition.z);
-        }
+        }*/
 
 
     }
