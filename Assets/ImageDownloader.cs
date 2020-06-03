a84bb429c5994ee96ba03b58c7e37b0e5caa40b4
Daria Ishchenko <54391498+Nekttuman@users.noreply.github.com>
2020-06-03 16:11:56 +0900

    -

---
+++ Assets/ImageDownloader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageDownloader : MonoBehaviour
+{
+    public string url = "https://docs.unity3d.com/uploads/Main/ShadowIntro.png";
+
+    IEnumerator Start()
+    {
+        Texture2D tex;
+        tex = new Texture2D(4, 4, TextureFormat.DXT1, false);
+        using (WWW www = new WWW(url))
+        {
+            yield return www;
+            www.LoadImageIntoTexture(tex);
+            GetComponent<Renderer>().material.mainTexture = tex;
+        }
+    }
+}
---
+++ Assets/UrlTester.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UrlTester : MonoBehaviour
+{
+    
+    public void Targets()
+    {
+       // Help.BrowseURL("https://drive.google.com/drive/folders/1jKKVLRCxv5E-3vnko9oapndXl3IfJQ6Y?usp=sharing");
+        Application.OpenURL("https://drive.google.com/drive/folders/1jKKVLRCxv5E-3vnko9oapndXl3IfJQ6Y?usp=sharing");
+    }
+
+
+}
